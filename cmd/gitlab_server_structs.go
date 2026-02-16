0e6346c12252ccfd8573f4960064172ee3f2fa13
Brianna Becker <bbecker@seatgeek.com>
2026-02-16 14:05:19 +0100

    Add objectKind

--- cmd/gitlab_server_structs.go
+++ cmd/gitlab_server_structs.go
@@ -2,6 +2,7 @@ package cmd
 
 type GitlabWebhookPayload struct {
 	EventType        string                                `json:"event_type"`
+	ObjectKind       string                                `json:"object_kind"`                 // "object_kind" is sent e.g. "pipeline" when event_type is omitted
 	Project          GitlabWebhookPayloadProject           `json:"project"`                     // "project" is sent for all events
 	ObjectAttributes *GitlabWebhookPayloadObjectAttributes `json:"object_attributes,omitempty"` // "object_attributes" is sent on "merge_request" events and "pipeline" events
 	MergeRequest     *GitlabWebhookPayloadMergeRequest     `json:"merge_request,omitempty"`     // "merge_request" is sent on "note" activity and "pipeline" events
@@ -18,7 +19,10 @@ type GitlabWebhookPayloadObjectAttributes struct {
 }
 
 func (o *GitlabWebhookPayloadObjectAttributes) GetCommitID() string {
-	if o.LastCommit != nil {
+	if o == nil {
+		return ""
+	}
+	if o.LastCommit.ID != "" {
 		return o.LastCommit.ID
 	}
 	return o.Commit.ID
@@ -27,6 +31,13 @@ func (o *GitlabWebhookPayloadObjectAttributes) GetCommitID() string {
 type GitlabWebhookPayloadMergeRequest struct {
 	IID        int                        `json:"iid"`
 	LastCommit GitlabWebhookPayloadCommit `json:"last_commit"`
+}
+
+func (m *GitlabWebhookPayloadMergeRequest) GetCommitID() string {
+	if m == nil {
+		return ""
+	}
+	return m.LastCommit.ID
 }
 
 type GitlabWebhookPayloadCommit struct {
